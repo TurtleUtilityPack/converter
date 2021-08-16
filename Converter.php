832893a4042a8793b0243bc264d157761726749d
BariPlayzYT <rifatbari28@gmail.com>
2021-08-16 21:12:20 +0600

    PHP: Conversion & Textures Config now functional

--- Converter.php
+++ Converter.php
@@ -9,14 +9,6 @@ use function is_writeable;
 use function json_decode;
 use function json_encode;
 
-/*
- * Todo List:
- *
- * - Actually rename, and move files.
- * - Make a way for customizeable file path conversions.
- * - Think about what I didn't add here.
- *
- */
 
 class Converter
 {
@@ -68,9 +60,13 @@ class Converter
      */
     public string $items_path_java = "assets/minecraft/textures/item";
 
+    /**
+     * @var array|string[][]
+     */
+    public array $registered_paths = ["item" => ["bedrock" => "textures/items", "java" => "assets/minecraft/textures/item"], "block" => ["bedrock" => "textures/blocks", "java" => "assets/minecraft/textures/block"]];
+
     public function __construct()
     {
-
 
         if(is_readable("textures.txt"))
         {
@@ -78,12 +74,21 @@ class Converter
             $contents = file_get_contents("textures.txt");
             $this->textures_bedrock = explode("\n", $contents);
 
+            $count = 0;
+
+            foreach($this->textures_bedrock as $shit)
+            {
+                $this->textures_bedrock[$count] = trim($shit);
+
+                $count++;
+            }
+
         }
 
 
     }
 
-    function config(): bool
+    public function config(): bool
     {
 
         if (is_writeable('config.json')) {
@@ -209,10 +214,93 @@ class Converter
 
             $converter_folder_name = $converted_packs . DIRECTORY_SEPARATOR . $folder;
             $converter_texture_folder = $converter_folder_name . DIRECTORY_SEPARATOR . "textures";
+            $folder_converted = $converted_packs . DIRECTORY_SEPARATOR . $folder . "-Converted";
+
+            @mkdir($folder_converted);
+
+            if($this->from == 'bedrock')
+            {
+
+                copy($converter_folder_name . DIRECTORY_SEPARATOR . "pack_icon.png", $folder_converted . DIRECTORY_SEPARATOR . "pack.png");
+
+                $manifest = json_decode(file_get_contents($converter_folder_name . DIRECTORY_SEPARATOR . "manifest.json"));
+
+                $mcmeta = new \stdClass();
+                $mcmeta->pack = ['pack_format' => $manifest->format_version, 'description' => $manifest->header->description];
+
+                $mcmetaHandle = fopen($folder_converted . DIRECTORY_SEPARATOR . "pack.mcmeta", "w+");
+                fwrite($mcmetaHandle, json_encode($mcmeta, JSON_PRETTY_PRINT));
+                fclose($mcmetaHandle);
+
+                @mkdir($folder_converted . DIRECTORY_SEPARATOR . "assets");
+                @mkdir($folder_converted . DIRECTORY_SEPARATOR . "assets" . DIRECTORY_SEPARATOR . "minecraft");
+                @mkdir($folder_converted . DIRECTORY_SEPARATOR . "assets" . DIRECTORY_SEPARATOR . "minecraft" . DIRECTORY_SEPARATOR . "textures");
+                @mkdir($folder_converted . DIRECTORY_SEPARATOR . $this->items_path_java);
+                @mkdir($folder_converted . DIRECTORY_SEPARATOR . "assets" . DIRECTORY_SEPARATOR . "minecraft" . DIRECTORY_SEPARATOR . "textures" . DIRECTORY_SEPARATOR . "block");
+
+                $textures = $this->textures_bedrock;
+
+                var_dump($textures);
+
+                foreach($textures as $texture)
+                {
+
+                    $languagizedTexture = $this->languagize($texture);
+
+
+                    if(strpos($languagizedTexture->bedrock, "{color}")) {
+
+                        foreach($this->colors as $color)
+                        {
+                            $bedrock = str_replace("{color}", $color, $languagizedTexture->bedrock);
+                            $java = str_replace("{color}", $color, $languagizedTexture->java);
+
+                            copy($converter_folder_name . DIRECTORY_SEPARATOR . $this->registered_paths[$languagizedTexture->type]["bedrock"] . DIRECTORY_SEPARATOR . $bedrock . ".png", $folder_converted . DIRECTORY_SEPARATOR . $this->registered_paths[$languagizedTexture->type]["java"] . DIRECTORY_SEPARATOR . $java . ".png");
+
+                        }
+
+
+                    } else {
+
+                        copy($converter_folder_name . DIRECTORY_SEPARATOR . $this->registered_paths[$languagizedTexture->type]["bedrock"] . DIRECTORY_SEPARATOR . $languagizedTexture->bedrock . ".png", $folder_converted . DIRECTORY_SEPARATOR . $this->registered_paths[$languagizedTexture->type]["java"] . DIRECTORY_SEPARATOR . $languagizedTexture->java . ".png");
+
+                    }
+                }
+
+            }
 
 
 
         }
+    }
+
+    function languagize($texture): \stdClass {
+
+        $daArray = new \stdClass();
+
+        $daArray->type = $this->getStringBetween($texture, '[', ']');
+        $daArray->full = str_replace("[$daArray->type]", "", $texture);
+        $daArray->non_equals = explode(" = ", $daArray->full);
+        $daArray->bedrock = $daArray->non_equals[0];
+        $daArray->java = $daArray->non_equals[1];
+
+
+
+        return $daArray;
+
+    }
+
+    /**
+     * @param $str
+     * @param $from
+     * @param $to
+     * @return string
+     * Gets string between $from and $to
+     */
+    function getStringBetween($str, $from, $to): string
+    {
+        $sub = substr($str, strpos ($str,$from) + strlen($from) , strlen ($str));
+        return substr($sub,0, strpos ($sub, $to));
     }
 
     function numericCheck($number) {
